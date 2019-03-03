67377db70986c7848c1d1566c6282283fa0031db
vivek anand y <y.vivekanand@gmail.com>
2019-03-03 23:57:44 +0530

    Validated Tel Input

---
+++ src/TelInputValidator.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable */
+import React from 'react';
+import IntlTelInput from 'react-intl-tel-input';
+/* eslint-enable */
+
+import { ValidatorComponent } from 'react-form-validator-core-vivek';
+import TextValidator from './TextValidator';
+import {FormControl,FormHelperText} from '@material-ui/core'
+import 'react-intl-tel-input/dist/main.css';
+
+
+export default class ValidatedTelInput extends ValidatorComponent {
+    constructor(props)
+    {
+        super(props);
+        this.state = {...this.state,isPhoneValid:true
+        }
+    }
+
+    onPhoneNumberChange = (isValid,enteredNumber,dialCodeObject) => {
+        if(enteredNumber.length === 0)
+        {
+            this.setState({isPhoneValid:true});
+        }
+        else{
+            this.setState({isPhoneValid:isValid});
+        }   
+        this.setChanged(true);
+        if(this.props.onPhoneNumberChange)
+        {
+            this.props.onPhoneNumberChange(isValid,enteredNumber,dialCodeObject)
+        }
+    }
+
+    onPhoneBlur = (isValid,enteredNumber,dialCodeObject) => {
+        this.setBlurred(true);
+        if(this.props.onPhoneNumberBlur)
+        {
+            this.props.onPhoneNumberBlur(isValid,enteredNumber,dialCodeObject);
+        }
+    }
+
+    phoneValid = (isValid) => {
+
+    }
+
+    render() {
+        /* eslint-disable no-unused-vars */
+        const {
+            error,
+            errorMessages,
+            validators,
+            requiredError,
+            helperText,
+            validatorListener,
+            withRequiredValidator,
+            onPhoneNumberChange,onPhoneNumberBlur,
+            ...rest
+        } = this.props;
+
+
+        const { isValid,changed,blurred } = this.state;
+        return (
+            <FormControl
+            error={(!isValid && changed && blurred)||(!this.state.isPhoneValid && changed && blurred)}
+            
+        >
+    <IntlTelInput
+      onPhoneNumberChange={this.onPhoneNumberChange}
+      onPhoneNumberBlur={this.onPhoneBlur}
+      {...rest}
+    />
+    <TextValidator  value={this.state.isPhoneValid?1:-1} style = {{display:"none"}} validators = {["isPositive"]} errorMessages= {['invalid PhoneNumber']}/>
+
+            <FormHelperText> {(!isValid && changed && blurred && this.getErrorMessage())||(!this.state.isPhoneValid && changed && blurred && "Invalid ph")  || helperText} </FormHelperText>
+            </FormControl>
+        );
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { ValidatorComponent, ValidatorForm } from 'react-form-validator-core-viv
 import SelectValidator from './SelectValidator';
 import TextValidator from './TextValidator';
 import RadioGroupValidator from './RadioGroupValidator';
+import TelInputValidator  from './TelInputValidator';
 
 exports.SelectValidator = SelectValidator;
 exports.TextValidator = TextValidator;
 exports.RadioGroupValidator = RadioGroupValidator;
+exports.TelInputValidator = TelInputValidator;
 
 exports.ValidatorComponent = ValidatorComponent;
 exports.ValidatorForm = ValidatorForm;
